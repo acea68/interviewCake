0399f582a079671656df568a5857928b19a4c953
acea68 <alancea01@gmail.com>
2022-07-31 09:28:03 -0500

    updates int cake

--- ic_2HashingAndHashTables/ic_palindromes/hasPermutation.js
+++ ic_2HashingAndHashTables/ic_palindromes/hasPermutation.js
@@ -1,31 +1,79 @@
-/* Write an efficient function that checks whether any permutation of an input string is a palindrome.
+/* Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome. ↴
+
 You can assume the input string only contains lowercase letters.
+
 Examples:
+
 "civic" should return true
 "ivicc" should return true
 "civil" should return false
 "livci" should return false
 "But 'ivicc' isn't a palindrome!"
+
 If you had this thought, read the question again carefully. We're asking if any permutation of the string is a palindrome. Spend some extra time ensuring you fully understand the question before starting. Jumping in with a flawed understanding of the problem doesn't look good in an interview. */
-function hasPalindromePermutation(theString) {
-  // Check if any permutation of the input is a palindrome
+
+// function hasPalindromePermutation(string) {
+//   let map = {};
+//   for (let char of string) {
+//     if (!map[char]) {
+//       map[char] = 1
+//     } else {
+//       map[char]++;
+//     }
+//   }
+//   let isOddCount = 0;
+//   for (let key in map) {
+//     if (map[key] % 2 !== 0) isOddCount++
+//   }
+//   if (isOddCount > 1) return false;
+//   return true;
+// }
+
+function hasPalindromePermutation(string) {
   let map = {};
-  for (let char of theString) {
+  for (let char of string) {
     if (map[char]) {
-      map[char] = 1;
+      delete map[char];
     } else {
-      map[char] += 1;
+      map[char] = 1;
     }
   }
-  let oddCount = 0;
-  for (let letter in map) {
-    if (map[letter] % 2 === 1) oddCount++;
-  }
-  if (oddCount > 1) return false
-  return true;
+  return Object.keys(map) < 2;
 }
-// var result = hasPalindromePermutation('ivicc');
-// console.log('🚀 ~ result:', result);
+
+// function hasPalindromePermutation(theString) {
+
+//   // Track characters we've seen an odd number of times
+//   const unpairedCharacters = new Set();
+
+//   for (let char of theString) {
+//     if (unpairedCharacters.has(char)) {
+//       unpairedCharacters.delete(char);
+//     } else {
+//       unpairedCharacters.add(char);
+//     }
+//   }
+
+//   // The string has a palindrome permutation if it
+//   // has one or zero characters without a pair
+//   return unpairedCharacters.size <= 1;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
