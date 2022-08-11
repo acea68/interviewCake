3c69c4b437084b58111bcacefaf595c35b46ca60
acea68 <alancea01@gmail.com>
2022-08-10 22:26:22 -0500

    updates findDuplicate

--- ic_5TreesAndGraphs/findDuplicate/findDuplicate.js
+++ ic_5TreesAndGraphs/findDuplicate/findDuplicate.js
@@ -24,21 +24,71 @@ Using this, find a duplicate integer in O(n) time while keeping our space cost a
 Drawing pictures will help a lot with this one. Grab some paper and pencil (or a whiteboard, if you have one). */
 
 function findDuplicate(intArray) {
-  let arrLen = intArray.length;
-  let lastElem = arrLen - 1;
-  let currNode = intArray[lastElem];
-  let cycleEnd = cycleStart = index = 0;
-  // while (index < intArray.length) {
-  for (let i = 0; i < arrLen; i++) {
-    currNode = intArray[currNode];
+
+  const n = intArray.length - 1;
+
+  // STEP 1: GET INSIDE A CYCLE
+  // Start at position n+1 and walk n steps to
+  // find a position guaranteed to be in a cycle
+  let positionInCycle = n + 1;
+  for (let i = 0; i < n; i++) {
+
+    // We subtract 1 from the current position to step ahead:
+    // the 2nd *position* in an array is *index* 1
+    positionInCycle = intArray[positionInCycle - 1];
   }
 
-  let recallPosition = intArray[currNode + 1];
-  let cycleCounter = 1;
-  while (currNode !== recallPosition) {
-    cycleCounter++;
+  // STEP 2: FIND THE LENGTH OF THE CYCLE
+  // Find the length of the cycle by remembering a position in the cycle
+  // and counting the steps it takes to get back to that position
+  const rememberedPositionInCycle = positionInCycle;
+  let currentPositionInCycle = intArray[positionInCycle - 1];  // 1 step ahead
+  let cycleStepCount = 1;
+
+  while (currentPositionInCycle !== rememberedPositionInCycle) {
+    currentPositionInCycle = intArray[currentPositionInCycle - 1];
+    cycleStepCount += 1;
   }
+
+  // STEP 3: FIND THE FIRST NODE OF THE CYCLE
+  // Start two pointers
+  //   (1) at position n+1
+  //   (2) ahead of position n+1 as many steps as the cycle's length
+  let pointerStart = n + 1;
+  let pointerAhead = n + 1;
+  for (let i = 0; i < cycleStepCount; i++) {
+    pointerAhead = intArray[pointerAhead - 1];
+  }
+
+  // Advance until the pointers are in the same position
+  // which is the first node in the cycle
+  while (pointerStart !== pointerAhead) {
+    pointerStart = intArray[pointerStart - 1];
+    pointerAhead = intArray[pointerAhead - 1];
+  }
+
+  // Since there are multiple values pointing to the first node
+  // in the cycle, its position is a duplicate in our array
+  return pointerStart;
 }
+
+
+// function findDuplicate(intArray) {
+//   let arrLen = intArray.length;
+//   let lastElem = arrLen - 1;
+//   let currNode = intArray[lastElem];
+//   let cycleEnd = cycleStart = index = 0;
+//   // while (index < intArray.length) {
+//   for (let i = 0; i < arrLen; i++) {
+//     currNode = intArray[currNode];
+//   }
+
+//   let recallPosition = intArray[currNode + 1];
+//   let cycleCounter = 1;
+//   while (currNode !== recallPosition) {
+//     cycleCounter++;
+//   }
+// }
 
 // function findDuplicate(intArray) {
 //   // Find a number that appears more than once ... in O(n) time
